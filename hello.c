b3e700e23238fd7bb81cf61104137d6d47b97bc7
Vicky <vicky048@gmail.com>
2018-06-27 00:10:09 +0530

    Version 1

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+}
+
